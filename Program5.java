74086f6e8f53f68bd0d7693eea61d377ccc1ca5e
hanbanty <mohakupmanyue@gmail.com>
2024-08-16 09:17:11 -0700

    Update Program5.java
    finished it

--- Program5.java
+++ Program5.java
@@ -1,9 +1,34 @@
 //import stuff here?
 
 //Your code here
+ public class Program5{
+     public static void main(String[] args){
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        int koopaKingMiles = 412;
+        int koopaKingGallons = 40;
+        int pipeFrameMiles = 361;
+        int pipeFrameGallons = 18;
+        int badwagonMiles = 161;
+        int badwagonGallons = 11;
+        
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:\n");
+        System.out.println("Royale averaged: "+ (double) royaleMiles/royaleGallons+" mpg");
+        System.out.println("Koopa King averaged: "+(double) koopaKingMiles/koopaKingGallons+" mpg");
+        System.out.println("Pipe Frame: "+(double) pipeFrameMiles/pipeFrameGallons+" mpg");
+        System.out.println("Badwagon: "+(double) badwagonMiles/badwagonGallons+" mpg");
+        
+    }
+}
 
 //Paste console output below:
 /*
+Mushroom Cup Prix Racer Average Miles/Per Gallon:
+
+Royale averaged: 31.77777777777778 mpg
+Koopa King averaged: 10.3 mpg
+Pipe Frame: 20.055555555555557 mpg
+Badwagon: 14.636363636363637 mpg
 
 
 */
